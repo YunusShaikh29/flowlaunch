5c0f3de632f37694cd77258b44bdc1650ca24ef6
Yunus Shaikh <yunusshaikh29082@gmail.com>
2024-05-30 21:57:50 +0530

    assignment done

--- app/page.js
+++ app/page.js
@@ -1,16 +1,16 @@
-'use client';
+"use client";
 
-import React, { useState, useEffect } from 'react';
-import ProductCard from '../components/ProductCard';
-import ProductModal from '../components/ProductModal';
+import React, { useState, useEffect } from "react";
+import ProductCard from "../components/ProductCard";
+import ProductModal from "../components/ProductModal";
 
 const HomePage = () => {
   const [products, setProducts] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const [selectedProduct, setSelectedProduct] = useState(null);
 
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
+    fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
@@ -29,12 +29,22 @@ const HomePage = () => {
         onChange={(e) => setSearchTerm(e.target.value)}
       />
       <div className="flex flex-wrap justify-center">
-        {filteredProducts.map((product) => (
-          <ProductCard key={product.id} product={product} onClick={() => setSelectedProduct(product)} />
-        ))}
+        {filteredProducts.map((product) =>
+          filteredProducts.length !== 0 && (
+            <ProductCard
+              key={product.id}
+              product={product}
+              onClick={() => setSelectedProduct(product)}
+            />
+          )
+        )}
+       {filteredProducts.length === 0 && <div className="text-2xl md:text-5xl font-bold">No product found</div>}
       </div>
       {selectedProduct && (
-        <ProductModal product={selectedProduct} onClose={() => setSelectedProduct(null)} />
+        <ProductModal
+          product={selectedProduct}
+          onClose={() => setSelectedProduct(null)}
+        />
       )}
     </div>
   );
